3f6bdb62d312d9786c1063817aab3e3cf36e7561
marbeydotdev <contact@marbey.dev>
2025-01-30 14:04:49 +0100

    +

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter_static from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 let projects = [
@@ -17,7 +17,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter_static(),
+		adapter: adapter(),
 		prerender: {
 			entries: [
 				"*",
